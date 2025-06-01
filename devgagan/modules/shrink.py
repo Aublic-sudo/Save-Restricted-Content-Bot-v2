b61bf880b66407a53ec732a7780c546b6578fb2c
Aublic-sudo <rstock937@gmail.com>
2025-06-01 10:24:12 +0530

    Update shrink.py

--- devgagan/modules/shrink.py
+++ devgagan/modules/shrink.py
@@ -73,9 +73,9 @@ async def token_handler(client, message):
     msg = await app.get_messages(chat_id, 796)
     user_id = message.chat.id
     if len(message.command) <= 1:
-        image_url = "https://i.postimg.cc/v8q8kGyz/startimg-1.jpg"
+        image_url = "https://ibb.co/Xf8PhswN"
         # Join Channel button removed as requested
-        premium = InlineKeyboardButton("Get Premium", url="https://t.me/kingofpatal")   
+        premium = InlineKeyboardButton("Get Premium", url="https://t.me/Aublic")   
         keyboard = InlineKeyboardMarkup([
             [premium]    
         ])
