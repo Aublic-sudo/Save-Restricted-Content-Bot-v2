48613be3de77f0015ae12aa6fdf8c7322bf63f86
Aublic-sudo <rstock937@gmail.com>
2025-06-01 09:26:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,17 +13,17 @@ YTUB_COOKIES = """
 # write here yt cookies
 """
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
+API_ID = int(getenv("API_ID", "25352517"))
+API_HASH = getenv("API_HASH", "2b7e6cf7752c3af91f958d67793a3e0b")
+BOT_TOKEN = getenv("BOT_TOKEN", "8025159606:AAGXRIe53L3jj9vS0-wtGEy_pTq7p_Yd1pc")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "7360968885").split()))
 MONGO_DB = getenv("MONGO_DB", "")
-LOG_GROUP = getenv("LOG_GROUP", "")
-CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
-FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "0"))
-PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "500"))
+LOG_GROUP = getenv("LOG_GROUP", "-1002659745442")
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002698150766"))
+FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "999999"))
+PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "999999"))
 WEBSITE_URL = getenv("WEBSITE_URL", "upshrink.com")
-AD_API = getenv("AD_API", "52b4a2cf4687d81e7d3f8f2b7bc2943f618e78cb")
+AD_API = getenv("AD_API", "")
 STRING = getenv("STRING", None)
 YT_COOKIES = getenv("YT_COOKIES", YTUB_COOKIES)
 DEFAULT_SESSION = getenv("DEFAUL_SESSION", None)  # added old method of invite link joining
